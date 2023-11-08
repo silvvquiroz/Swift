dc5bead16d45180b2cd61114600f15e6a9f4720d
silvvquiroz <149844440+silvvquiroz@users.noreply.github.com>
2023-11-08 16:01:19 -0500

    Add files via upload

---
+++ Loops/Loops/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Loops
+//
+//  Created by luser on 11/6/23.
+//
+
+import Foundation
+
+// while
+var num = 0
+while num < 10 {
+    print(num)
+    num += 1
+}
+
+// for
+var numeros = [1,2,3,4,5]
+for num in numeros {
+    print(num)
+}
+
+for num in 1 ... 5 {
+    print(num)
+}
