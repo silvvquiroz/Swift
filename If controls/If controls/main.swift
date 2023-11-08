5164db5a712cda995e2bbf1f0ac92035416e3946
silvvquiroz <149844440+silvvquiroz@users.noreply.github.com>
2023-11-08 16:02:05 -0500

    Add files via upload

---
+++ If controls/If controls/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  If controls
+//
+//  Created by luser on 11/7/23.
+//
+
+import Foundation
+
+var edad = 30
+if edad < 30 {
+   print("30 - ")
+}
+else if edad == 30 {
+   print("30 = ")
+}
+else {
+   print("30 + ")
+}
