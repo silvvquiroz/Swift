53d7830f279cbabc1461f5173da1c2db436ad32b
silvvquiroz <149844440+silvvquiroz@users.noreply.github.com>
2023-11-08 16:01:47 -0500

    Add files via upload

---
+++ Funciones/Funciones/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Funciones
+//
+//  Created by luser on 11/7/23.
+//
+
+import Foundation
+
+// definición
+func myFunction() {
+    print("holi")
+}
+
+myFunction()
+
+// input y output
+func sumar(x: Int, y: Int) -> Int {
+    //print(x+y)
+    return x+y
+}
+
+var suma = sumar(x:1, y:2)
+print("suma: ", suma)
