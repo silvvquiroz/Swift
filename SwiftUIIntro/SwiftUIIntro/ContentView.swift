431359c3e5cd448ed97d7a9b718037bd7d3a0cba
silvvquiroz <149844440+silvvquiroz@users.noreply.github.com>
2023-11-10 21:53:09 -0500

    Add files via upload

---
+++ SwiftUIIntro/SwiftUIIntro/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIIntro
+//
+//  Created by luser on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Text("Texto 2")
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIIntro/SwiftUIIntro/SwiftUIIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntroApp.swift
+//  SwiftUIIntro
+//
+//  Created by luser on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
